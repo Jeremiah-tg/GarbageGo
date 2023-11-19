f759984e595edfe41b3abb20cbae75c73bd9327e
Jeremiah Joseph <68328072+Jeremiah-tg@users.noreply.github.com>
2023-11-19 06:11:25 +0300

    second Update

--- order.php
+++ order.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  <link type="image/x-icon" rel="icon" href="img/11.ico">
 </head>
 <body>
     <header>
@@ -20,10 +19,10 @@
             </ul>
         </nav>
     </header>
-
+    
 
     <div class="container mt-5">
-
+        
         <div class="row" style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
             <h1 class="mt-5 text-center">Order for service</h1>
         <div class="col-lg-2"></div>
@@ -58,7 +57,7 @@
         </div>
         <div class="col-lg-2"></div>
     </div>
-
+        
     </div>
     <footer>
         <p>&copy; 2023 Garbage Go. All rights reserved.</p>
